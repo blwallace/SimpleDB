be8a9a56af4455941afa7dfd822ffa8049578e6a
blwallace <blwallace@gmail.com>
2016-01-29 12:50:39 -0600

    predicate first

--- CS186-proj1/src/java/simpledb/Predicate.java
+++ CS186-proj1/src/java/simpledb/Predicate.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
  * Predicate compares tuples to a specified Field value.
  */
 public class Predicate implements Serializable {
+    
 
     private static final long serialVersionUID = 1L;
 
