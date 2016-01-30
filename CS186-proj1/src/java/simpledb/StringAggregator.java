c429504167fa70f48c8fa3ba798f505c87731275
blwallace <blwallace@gmail.com>
2016-01-30 12:11:51 -0600

    2nd pull stringagg

--- CS186-proj1/src/java/simpledb/StringAggregator.java
+++ CS186-proj1/src/java/simpledb/StringAggregator.java
@@ -1,11 +1,23 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
 public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+    private int gbfield;
+    private int afield;
+    private Op what;
+    private boolean isOpen;
+    private TupleDesc td;
+    // create hashmaps to store aggregate values
+    private HashMap<Field,Field> strFieldCount;
 
     /**
      * Aggregate constructor
@@ -17,7 +29,12 @@ public class StringAggregator implements Aggregator {
      */
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+        this.gbfield = gbfield;
+        this.afield = afield;
+        this.what = what;
+        this.strFieldCount = new HashMap<Field,Field>();
+        this.isOpen = false;
+
     }
 
     /**
@@ -25,7 +42,53 @@ public class StringAggregator implements Aggregator {
      * @param tup the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+        Field tupleGroup;
+
+        // variables for our td
+        Type[] typeAr;
+        String[] fieldAr;
+
+        if(!isOpen){
+            isOpen = true;
+            if(gbfield != Aggregator.NO_GROUPING){
+                Type groupType = tup.getTupleDesc().getFieldType(gbfield);
+                String groupField = tup.getTupleDesc().getFieldName(gbfield);
+                typeAr = new Type[]{groupType, Type.INT_TYPE};
+                fieldAr = new String[]{groupField, tup.getTupleDesc().getFieldName(afield)};
+            }
+            else{
+                fieldAr = new String[]{tup.getTupleDesc().getFieldName(afield)};
+                typeAr = new Type[]{Type.INT_TYPE};
+            }
+            td = new TupleDesc(typeAr,fieldAr);
+        }
+
+        // grouping logic
+        if(gbfield == Aggregator.NO_GROUPING){
+            tupleGroup = new IntField(Aggregator.NO_GROUPING);
+        }
+        else {
+            tupleGroup = tup.getField(gbfield);
+        }
+
+        IntField tga = (IntField)tupleGroup;
+        int tg = tga.getValue();
+        IntField tv = (IntField) strFieldCount.get(tga);
+
+        if(what != Op.COUNT){
+            throw new IllegalArgumentException();
+        }
+        else{
+                // if the value doesn't exist in the hashmap, put it in the hashmap
+            if(strFieldCount.get(tupleGroup) == null){
+                strFieldCount.put(tupleGroup,new IntField(1));
+                }
+                // else we add 1 to the existing tuple sum
+            else{
+                strFieldCount.put(tupleGroup,new IntField(tv.getValue() + 1));
+                }
+        }
+
     }
 
     /**
@@ -38,7 +101,24 @@ public class StringAggregator implements Aggregator {
      */
     public DbIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for proj2");
+        Iterator iterator = strFieldCount.entrySet().iterator();
+        ArrayList tupleIt = new ArrayList<Tuple>();
+
+        while(iterator.hasNext()){
+            Map.Entry pair = (Map.Entry)iterator.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            Tuple tup = new Tuple(td);
+            if(Aggregator.NO_GROUPING == gbfield){
+                tup.setField(0, (Field) pair.getValue());
+            }
+            else{
+                tup.setField(0, (Field) pair.getKey());
+                tup.setField(1, (Field) pair.getValue());
+            }
+            tupleIt.add(tup);
+        }
+        TupleIterator finalIt = new TupleIterator(td,tupleIt);
+        return finalIt;
     }
 
 }
