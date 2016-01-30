0a907dfd2182006a7c8bea0fbd865a12e040453b
blwallace <blwallace@gmail.com>
2016-01-30 13:44:13 -0600

    agg

--- CS186-proj1/src/java/simpledb/Aggregate.java
+++ CS186-proj1/src/java/simpledb/Aggregate.java
@@ -1,6 +1,6 @@
 package simpledb;
 
-import java.util.*;
+import java.util.NoSuchElementException;
 
 /**
  * The Aggregation operator that computes an aggregate (e.g., sum, avg, max,
@@ -10,15 +10,22 @@ import java.util.*;
 public class Aggregate extends Operator {
 
     private static final long serialVersionUID = 1L;
+    private DbIterator child;
+    private DbIterator aggIterator;
+    private int afield;
+    private int gfield;
+    private Aggregator.Op aop;
+    private Aggregator aggregator;
+    private TupleDesc td;
 
     /**
      * Constructor.
-     * 
+     *
      * Implementation hint: depending on the type of afield, you will want to
      * construct an {@link IntAggregator} or {@link StringAggregator} to help
      * you with your implementation of readNext().
-     * 
-     * 
+     *
+     *
      * @param child
      *            The DbIterator that is feeding us tuples.
      * @param afield
@@ -30,7 +37,34 @@ public class Aggregate extends Operator {
      *            The aggregation operator to use
      */
     public Aggregate(DbIterator child, int afield, int gfield, Aggregator.Op aop) {
-	// some code goes here
+        this.child = child;
+        this.afield = afield;
+        this.gfield = gfield;
+        this.aop = aop;
+
+        // variables for our td and building our td
+        Type[] typeAr;
+        String[] fieldAr;
+        if(gfield != Aggregator.NO_GROUPING){
+            Type groupType = child.getTupleDesc().getFieldType(gfield);
+            String groupField = child.getTupleDesc().getFieldName(gfield);
+            typeAr = new Type[]{groupType, Type.INT_TYPE};
+            fieldAr = new String[]{groupField, child.getTupleDesc().getFieldName(afield)};
+        }
+        else{
+            fieldAr = new String[]{child.getTupleDesc().getFieldName(afield)};
+            typeAr = new Type[]{Type.INT_TYPE};
+        }
+        td = new TupleDesc(typeAr,fieldAr);
+
+        // create our aggregator
+        if(td.getFieldType(afield) == Type.INT_TYPE){
+            aggregator = new IntegerAggregator(gfield,td.getFieldType(gfield),afield,aop);
+        }
+        else{
+            aggregator = new StringAggregator(gfield,td.getFieldType(gfield),afield,aop);
+        }
+
     }
 
     /**
@@ -39,8 +73,12 @@ public class Aggregate extends Operator {
      *         {@link simpledb.Aggregator#NO_GROUPING}
      * */
     public int groupField() {
-	// some code goes here
-	return -1;
+        if(gfield == Aggregator.NO_GROUPING){
+            return Aggregator.NO_GROUPING;
+        }
+        else{
+            return gfield;
+        }
     }
 
     /**
@@ -49,16 +87,20 @@ public class Aggregate extends Operator {
      *         null;
      * */
     public String groupFieldName() {
-	// some code goes here
-	return null;
+        if(gfield == Aggregator.NO_GROUPING){
+            return null;
+        }
+        else{
+            return child.getTupleDesc().getFieldName(gfield);
+        }
     }
 
     /**
      * @return the aggregate field
      * */
     public int aggregateField() {
-	// some code goes here
-	return -1;
+        // some code goes here
+        return -1;
     }
 
     /**
@@ -66,25 +108,40 @@ public class Aggregate extends Operator {
      *         tuples
      * */
     public String aggregateFieldName() {
-	// some code goes here
-	return null;
+        return child.getTupleDesc().getFieldName(afield);
+
     }
 
     /**
      * @return return the aggregate operator
      * */
     public Aggregator.Op aggregateOp() {
-	// some code goes here
-	return null;
+        return aop;
     }
 
     public static String nameOfAggregatorOp(Aggregator.Op aop) {
-	return aop.toString();
+        return aop.toString();
     }
 
     public void open() throws NoSuchElementException, DbException,
-	    TransactionAbortedException {
-	// some code goes here
+            TransactionAbortedException {
+        super.open();
+        child.open();
+        while(child.hasNext()){
+            aggregator.mergeTupleIntoGroup(child.next());
+        }
+        child.close();
+        aggIterator = aggregator.iterator();
+        aggIterator.open();
+    }
+
+    public boolean hasNext() throws DbException, TransactionAbortedException {
+        if(aggIterator.hasNext()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -95,12 +152,14 @@ public class Aggregate extends Operator {
      * aggregate. Should return null if there are no more tuples.
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-	// some code goes here
-	return null;
+        if(aggIterator.hasNext()){
+            return aggIterator.next();
+        }
+        else return null;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-	// some code goes here
+        aggIterator.rewind();
     }
 
     /**
@@ -108,30 +167,29 @@ public class Aggregate extends Operator {
      * this will have one field - the aggregate column. If there is a group by
      * field, the first field will be the group by field, and the second will be
      * the aggregate value column.
-     * 
+     *
      * The name of an aggregate column should be informative. For example:
      * "aggName(aop) (child_td.getFieldName(afield))" where aop and afield are
      * given in the constructor, and child_td is the TupleDesc of the child
      * iterator.
      */
     public TupleDesc getTupleDesc() {
-	// some code goes here
-	return null;
+        return td;
     }
 
     public void close() {
-	// some code goes here
+        aggIterator.close();
     }
 
     @Override
     public DbIterator[] getChildren() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
-	// some code goes here
+        // some code goes here
     }
-    
+
 }
