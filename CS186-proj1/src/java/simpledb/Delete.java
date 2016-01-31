a1e6297e446ed4aa62891b99268e75e7c1d3320b
blwallace <blwallace@gmail.com>
2016-01-31 12:22:35 -0600

    passed delete and insert test

--- CS186-proj1/src/java/simpledb/Delete.java
+++ CS186-proj1/src/java/simpledb/Delete.java
@@ -7,6 +7,13 @@ package simpledb;
 public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
+    TransactionId t;
+    DbIterator child;
+    TupleDesc td;
+    int ticker;
+    // boolean to determine if we did funciton multiple tiems
+    private boolean first_time;
+
 
     /**
      * Constructor specifying the transaction that this delete belongs to as
@@ -18,26 +25,38 @@ public class Delete extends Operator {
      *            The child operator from which to read tuples for deletion
      */
     public Delete(TransactionId t, DbIterator child) {
-        // some code goes here
+        this.t = t;
+        this.child = child;
+        this.td = new TupleDesc(new Type[]{Type.INT_TYPE},new String[]{"Deleted Records"});
+        ticker = 0;
+        first_time = true;
     }
 
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+        return td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
+        super.open();
+        child.open();
     }
 
     public void close() {
-        // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
+        child.rewind();
     }
+    // custom function to determine if there is another tuple
+    public boolean hasNext(){
+        if(ticker != 0 || !first_time){
+            return false;
+        }
 
+        return true;
+    }
     /**
      * Deletes tuples as they are read from the child operator. Deletes are
      * processed via the buffer pool (which can be accessed via the
@@ -48,7 +67,32 @@ public class Delete extends Operator {
      * @see BufferPool#deleteTuple
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-        // some code goes here
+        //create a tuple
+        Tuple tup = new Tuple(td);
+
+        //if opened mroe than once, return null
+        if(ticker != 0 || !first_time){
+            return null;
+        }
+
+        // determine if we can insert a tuple from child
+        while(child.hasNext()){
+            Database.getBufferPool().deleteTuple(t, child.next());
+            ticker++;
+        }
+        tup.setField(0,new IntField(ticker));
+        first_time = false;
+        return tup;
+    }
+    
+    public Tuple next(){
+        try {
+            return fetchNext();
+        } catch (TransactionAbortedException e) {
+            e.printStackTrace();
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
